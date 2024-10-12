67d1a41faaaf2f363379bef15be1db15f0163c22
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2024-10-12 18:19:54 -0300

    Improved test suit
    It looks neat and colorful
    
    Co-Authored-By: FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
    Co-Authored-By: Thomas Ruijgt <112571497+ThomasR62875@users.noreply.github.com>
    Co-Authored-By: ivanOdz <76888044+ivanOdz@users.noreply.github.com>

--- script/ubuntu/test.sh
+++ script/ubuntu/test.sh
@@ -7,21 +7,37 @@ cd "$BASE_PATH"
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+GREY='\033[1;30m'
 OFF='\033[0m'
 STATUS=0
+
+test_count=0
+pass_count=0
+fail_count=0
+skip_count=0
 
 echo "Compiler should accept..."
 echo ""
 
 accept_folder="src/test/c/accept/"
-for test in $(find "$accept_folder" -type f -not -path '*/.*'); do
+for test in $(find "$accept_folder" -type f); do
+	test_count=$((test_count + 1))
+
+	if [[ "$(basename "$test")" == .skip* ]]; then
+		skip_count=$((skip_count + 1))
+		echo -e "    ${test#$accept_folder}, ${GREY}skipped${OFF}"
+		continue
+	fi
+
 	cat "$test" | build/Compiler >/dev/null 2>&1
 	RESULT="$?"
 	if [ "$RESULT" == "0" ]; then
 		echo -e "    ${test#$accept_folder}, ${GREEN}and it does${OFF} (status $RESULT)"
+		pass_count=$((pass_count + 1))
 	else
 		STATUS=1
 		echo -e "    ${test#$accept_folder}, ${RED}but it rejects${OFF} (status $RESULT)"
+		fail_count=$((fail_count + 1))
 	fi
 done
 echo ""
@@ -30,17 +46,28 @@ echo "Compiler should reject..."
 echo ""
 
 reject_folder="src/test/c/reject/"
-for test in $(find "$reject_folder" -type f -not -path '*/.*'); do
+for test in $(find "$reject_folder" -type f); do
+	test_count=$((test_count + 1))
+
+	if [[ "$(basename "$test")" == .skip* ]]; then
+		skip_count=$((skip_count + 1))
+		echo -e "    ${test#$accept_folder}, ${GREY}skipped${OFF}"
+		continue
+	fi
+
 	cat "$test" | build/Compiler >/dev/null 2>&1
 	RESULT="$?"
 	if [ "$RESULT" != "0" ]; then
 		echo -e "    ${test#$reject_folder}, ${GREEN}and it does${OFF} (status $RESULT)"
+		pass_count=$((pass_count + 1))
 	else
 		STATUS=1
 		echo -e "    ${test#$reject_folder}, ${RED}but it accepts${OFF} (status $RESULT)"
+		fail_count=$((fail_count + 1))
 	fi
 done
 echo ""
 
 echo "All done."
+echo -e "Executed $GREEN$test_count$OFF tests, $GREEN$pass_count$OFF passed, $RED$fail_count$OFF failed, $GREY$skip_count$OFF skipped."
 exit $STATUS
